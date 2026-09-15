2e9ef160477de57e04efbf991d2331d5746774bd
Raxon Suri <121737678+RaxPlay@users.noreply.github.com>
2026-09-14 22:32:17 -0400

    Better Auth initial commit

---
+++ lib/auth.ts
@@ -0,0 +1,22 @@
+import { betterAuth } from "better-auth";
+import { drizzleAdapter } from "@better-auth/drizzle-adapter";
+import { db } from "@/app/index";
+import { nextCookies } from "better-auth/next-js";
+import { headers } from "next/headers";
+import "dotenv/config";
+
+export const auth = betterAuth({
+  database: drizzleAdapter(db, {
+    provider: "pg",
+  }),
+  // emailAndPassword: { Not sure if this should be added },
+  plugins: [nextCookies()],
+  pages: {
+    signIn: "/signin",
+  },
+  // socialProviders: { Will add Microsoft OAuth },
+});
+export const getSession = async () =>
+  auth.api.getSession({
+    headers: await headers(),
+  });
