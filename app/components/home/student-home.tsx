07d79eb6feb002da45e489ba4c3e3e13dce3a566
Raxon Suri <121737678+RaxPlay@users.noreply.github.com>
2026-09-15 23:36:43 -0400

    Student home page

--- app/components/home/student-home.tsx
+++ app/components/home/student-home.tsx
@@ -1,7 +1,9 @@
+import PassForm from "./student-related/pass-form";
+
 export default function StudentHome() {
   return (
-    <div>
-      
+    <div className="flex flex-col items-center">
+      <PassForm></PassForm>
     </div>
   )
 }
