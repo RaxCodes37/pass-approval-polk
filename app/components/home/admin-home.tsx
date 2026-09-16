9a37ef43e0d134a6596cbc6e4cdd639c2cead687
Raxon Suri <121737678+RaxPlay@users.noreply.github.com>
2026-09-15 23:36:05 -0400

    Admin home-page

--- app/components/home/admin-home.tsx
+++ app/components/home/admin-home.tsx
@@ -1,7 +1,16 @@
+import AdminNavbar from "./admin-related/admin-navbar";
+import DisplayRequests from "./admin-related/display-requests";
+
 export default function AdminHome() {
+  //Check again, if not admin -> back to student page
+
   return (
     <div>
-      
+      <AdminNavbar />
+
+      <div className="flex justify-center ">
+        <DisplayRequests />
+      </div>
     </div>
-  )
+  );
 }
