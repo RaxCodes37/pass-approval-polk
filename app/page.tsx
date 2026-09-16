86d4fbeb3a52e7d45364930c64653c8a131192a1
Raxon Suri <121737678+RaxPlay@users.noreply.github.com>
2026-09-15 23:35:27 -0400

    Redirection

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,5 @@
-import React from "react";
+import { redirect } from "next/navigation";
 
 export default function Home() {
-  return (
-    <div>
-      Hello world
-    </div>
-  );
+  redirect("/home")
 }
