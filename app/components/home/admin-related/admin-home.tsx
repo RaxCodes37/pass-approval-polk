cf9711a9d54bb1e229247f4004bcb46a3ba0725b
Raxon Suri <121737678+RaxPlay@users.noreply.github.com>
2026-09-17 00:51:25 -0400

    Removed unnecessary comments

--- app/components/home/admin-related/admin-home.tsx
+++ app/components/home/admin-related/admin-home.tsx
@@ -2,8 +2,6 @@ import AdminNavbar from "./admin-navbar";
 import DisplayRequests from "./display-requests";
 
 export default function AdminHome() {
-  //Check again, if not admin -> back to student page
-
   return (
     <div>
       <AdminNavbar />
--- app/home/page.tsx
+++ app/home/page.tsx
@@ -2,13 +2,12 @@ import { getSession } from "@/lib/auth";
 import { redirect } from "next/navigation";
 
 export default async function Home() {
-  // ! Add a check after implementing Microsoft OAuth
-  // If user.email contains "@mypolkschools.net" send to student page, else, send to admin.
   const session = await getSession();
 
   if (!session) redirect("/sign-in");
-  else {
-    if (session.user.email.includes("@mypolkschools.net")) redirect("/student");
-    else if (session.user.email.includes("@mypolkeducator.net"))redirect("/admin");
-  }
+  const userName: string = session.user.name;
+
+  if (session.user.email.includes("@mypolkschools.net")) redirect("/student");
+  else if (session.user.email.includes("@mypolkeducator.net"))
+    redirect(`/admin/${userName}`);
 }
