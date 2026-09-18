91cfca091898d4bdf485c8c05a08b27be1c38545
Raxon Suri <121737678+RaxPlay@users.noreply.github.com>
2026-09-17 22:21:41 -0400

    Schema Changes | changed default status

--- schema.ts
+++ schema.ts
@@ -9,7 +9,7 @@ export const passesTable = pgTable("passes", {
   reason: text("reason_for_pass").notNull(),
   timeOfDeparture: timestamp("time_of_departure").defaultNow().notNull(),
   timeOfReturn: timestamp("time_of_return"),
-  status: text("pass_status").default("not approved")
+  status: text("pass_status").default("pending")
 })
 
 export const user = pgTable("user", {
