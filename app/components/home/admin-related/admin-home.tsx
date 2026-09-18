dcebc42b84f96d7a66fb0691772db1276e1cdeb9
Raxon Suri <121737678+RaxPlay@users.noreply.github.com>
2026-09-17 22:22:55 -0400

    Implemented pass-request view & client interaction

--- app/components/home/admin-related/admin-home.tsx
+++ app/components/home/admin-related/admin-home.tsx
@@ -1,17 +1,52 @@
+"use client";
+
+import { useEffect, useState } from "react";
 import AdminNavbar from "./admin-navbar";
 import DisplayRequests from "./display-requests";
+import { acceptRequest, denyRequest, getPassRequest } from "@/utils/db-actions";
+import { PassRequest } from "@/utils/interfaces";
 
 interface Props {
-  teacherName: string
+  teacherName: string;
 }
 
-export default function AdminHome({teacherName}: Props) {
+export default function AdminHome({ teacherName }: Props) {
+  const [requests, setRequests] = useState<PassRequest[]>([]);
+
+  useEffect(() => {
+    const getStudentRequests = async () => {
+      setRequests(await getPassRequest(teacherName));
+    };
+
+    getStudentRequests();
+  }, []);
+
+  const acceptRequestFunction = async (passId: string) => {
+    setRequests(requests.filter((r) => r.passId !== passId));
+
+    try {
+      await acceptRequest(passId);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const denyRequestFunction = async (passId: string) => {
+    setRequests(requests.filter((r) => r.passId !== passId));
+
+    try {
+      await denyRequest(passId);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <div>
       <AdminNavbar />
 
-      <div className="flex justify-center ">
-        <DisplayRequests />
+      <div className="flex justify-center">
+        <DisplayRequests requests={requests} setRequests={setRequests} acceptRequestFunction={acceptRequestFunction} denyRequestFunction={denyRequestFunction}/>
       </div>
     </div>
   );
--- app/components/home/admin-related/display-requests.tsx
+++ app/components/home/admin-related/display-requests.tsx
@@ -1,6 +1,14 @@
+import { PassRequest } from "@/utils/interfaces";
 import React from "react";
 
-export default function DisplayRequests() {
+interface Props {
+  requests: PassRequest[];
+  setRequests: React.Dispatch<React.SetStateAction<PassRequest[]>>;
+  acceptRequestFunction: (passId: string) => void
+  denyRequestFunction: (passId: string) => void
+}
+
+export default function DisplayRequests({ requests, setRequests, acceptRequestFunction, denyRequestFunction }: Props) {
   return (
     <div
       className="mt-30 w-80 h-fit sm:w-100 border-2 border-[#5abbfc] bg-[#2b5d86] rounded-md flex flex-col items-center text-center"
@@ -9,8 +17,38 @@ export default function DisplayRequests() {
       <h1 className="text-xl font-semibold sm:text-2xl w-full py-3 rounded-t-sm bg-[#4F98C8] shadow-1md">
         All Requests
       </h1>
-      <div className="h-80 px-2">
-        <p>All requests will be displayed here</p>
+      <div className="min-h-80 h-fit w-full p-2 flex flex-col gap-2">
+        {
+          
+        }
+        {requests.map((request) => (
+          <div
+            key={request.passId}
+            className="flex flex-col border border-[#64bffb] bg-[#4F98C8] px-2 py-1 rounded-md"
+          >
+            <div className="flex justify-between">
+              <p className="underline">{request.studentName}</p>
+
+              <div>
+                <p className="text-[#2e6795]">
+                  From{" "}
+                  <span className="underline">{request.classDepartedFrom}</span>{" "}
+                  to <span className="underline">{request.destination}</span>
+                </p>
+              </div>
+            </div>
+
+            <div className="flex justify-between">
+              <p className="text-left">{request.reason}</p>
+              <p className="text-right text-[#2e6795]">{request.status}</p>
+            </div>
+
+            <div className="mt-2 mb-1 flex justify-between gap-4 self-center w-[60%]">
+              <button className="accept-request-btn" onClick={() => acceptRequestFunction(request.passId)}>Accept</button>
+              <button className="deny-request-btn" onClick={() => denyRequestFunction(request.passId)}>Deny</button>
+            </div>
+          </div>
+        ))}
       </div>
     </div>
   );
