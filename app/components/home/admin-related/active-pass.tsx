9ff7628e8ccc66794c3ed556df4f27675ddeb2de
Raxon Suri <121737678+RaxPlay@users.noreply.github.com>
2026-09-18 09:01:57 -0400

    Started active pass layout

--- app/components/home/admin-related/active-pass.tsx
+++ app/components/home/admin-related/active-pass.tsx
@@ -8,8 +8,59 @@ interface Props {
   setActiveRequests: React.Dispatch<React.SetStateAction<PassRequest[]>>;
 }
 
-export default function ActivePass({activeRequests, setActiveRequests}: Props) {
+export default function ActivePass({
+  activeRequests,
+  setActiveRequests,
+}: Props) {
   console.log(activeRequests);
 
-  return <div></div>;
+  return (
+    <div className="mt-20 text-center">
+      <h1 className="text-xl font-semibold sm:text-2xl w-full py-3 rounded-t-sm bg-[#4F98C8] border-2 border-b border-[#64bffb]">
+        Active Pass
+      </h1>
+      <table className="border-2 border-[#64bffb] bg-[#4F98C8] border-t-0 w-90 text-center">
+        <thead>
+          <tr className="text-center p-2">
+            <th className="border border-[#64bffb] border-t-0">Student Name</th>
+            <th className="p-2 border border-[#64bffb] border-t-0">Destination</th>
+            <th className="p-2 border border-[#64bffb] border-t-0">Reason</th>
+            <th className="p-2 border border-[#64bffb] border-t-0">Status</th>
+            {
+              // ! Same typing error as the one at 'display-all-requests.tsx', will fix later.
+              // <th className="p-2 border border-[#64bffb]">Departure Time</th>
+              // <th className="p-2 border border-[#64bffb]">Return Time</th>
+            }
+          </tr>
+        </thead>
+        <tbody>
+          {activeRequests.map((request) => (
+            <tr className="border border-[#64bffb] p-2" key={request.passId}>
+              <td className="border border-[#64bffb] bg-[#37739b] p-2">
+                {request.studentName}
+              </td>
+              <td className="border border-[#64bffb] bg-[#37739b] p-2">
+                {request.destination}
+              </td>
+              <td className="border border-[#64bffb] bg-[#37739b] p-2">
+                {request.reason}
+              </td>
+              <td className="border border-[#64bffb] bg-[#37739b] p-2">
+                {request.status}
+              </td>
+              {
+                // ! Part of typing error.
+                // <td className="border border-[#64bffb] bg-[#37739b] p-2">
+                //   {request.timeOfDeparture}
+                // </td>
+                // <td className="border border-[#64bffb] bg-[#37739b]p-2">
+                //   {request.timeOfReturn}
+                // </td>
+              }
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 }
