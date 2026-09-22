eda67719476f1d0ec29e8917fe7c44d1e94bc83d
Raxon Suri <121737678+RaxPlay@users.noreply.github.com>
2026-09-21 22:14:18 -0400

    Added detail

--- app/components/home/student-related/student-home.tsx
+++ app/components/home/student-related/student-home.tsx
@@ -12,6 +12,10 @@ export default function StudentHome({ studentName }: Props) {
 
   return (
     <div className="flex flex-col items-center">
+      <h1 className="text-xl font-semibold sm:text-2xl w-full py-6 rounded-b-md bg-[#4F98C8] text-center">
+        Welcome, <span className="underline">{studentName}</span>
+      </h1>
+
       <PassForm studentName={studentName} setMessage={setMessage} />
 
       {message === "" ? (
