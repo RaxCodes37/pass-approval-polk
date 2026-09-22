0855c26edff39088cd9ab0f88a5d745d42742cdc
Raxon Suri <121737678+RaxPlay@users.noreply.github.com>
2026-09-21 22:34:59 -0400

    Style fix 2

--- app/components/home/admin-related/active-pass.tsx
+++ app/components/home/admin-related/active-pass.tsx
@@ -13,11 +13,11 @@ export default function ActivePass({
   setActiveRequests,
 }: Props) {
   return (
-    <div className="mt-20 w-78 sm:w-124 text-center">
-      <h1 className="text-xl font-semibold sm:text-2xl w-117.25 py-3 rounded-t-sm bg-[#4F98C8] border-2 border-b border-[#64bffb]">
+    <div className="mt-20 w-78 sm:w-130 text-center">
+      <h1 className="text-xl font-semibold sm:text-2xl w-ful py-3 rounded-t-sm bg-[#4F98C8] border-2 border-b border-[#64bffb]">
         Active Pass
       </h1>
-      <table className="border-2 border-[#64bffb] bg-[#4F98C8] border-t-0 w-82.5 text-center">
+      <table className="border-2 border-[#64bffb] bg-[#4F98C8] border-t-0 w-full text-center">
         <thead>
           <tr className="text-center p-2">
             <th className="border border-[#64bffb] border-t-0">Student Name</th>
